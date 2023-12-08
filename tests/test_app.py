725b49912c0045237b8b9bdabb5281a8f798c30d
odiegofarias <diego.farias.fernandes@gmail.com>
2023-12-08 15:17:50 -0300

    Configurações do POST com schemas para criaçao de usuários

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,30 @@
+import pytest
 from fastapi.testclient import TestClient
 from fast_zero.app import app
 
-def test_root_deve_retirnar_200_e_ola_mundo():
-    client = TestClient(app)
+@pytest.fixture
+def client():
+    return TestClient(app)
 
+def test_root_deve_retirnar_200_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == 200
     assert response.json() == {'message': 'Olá, Mundo'}
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'Alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == 201
+    assert response.json() == {
+        'username': 'Alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
